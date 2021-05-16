ffbe7f8885846026db42268ca7353b9ec9a429eb
Proshenzit <37993972+Proshenzit@users.noreply.github.com>
2021-05-15 20:52:45 -0700

    Add files via upload

---
+++ 1/index.js
@@ -0,0 +1,55 @@
+var digit=prompt("Enter  your Digit:");
+
+if(digit == 0)
+
+console.log("zero");
+
+else if(digit==1)
+
+console.log("one");
+else if(digit==2)
+
+console.log("Two");
+else if(digit==3)
+
+console.log("three");
+else if(digit==4)
+
+console.log("Four");
+else if(digit==5)
+
+console.log("Five");
+else if(digit==6)
+
+console.log("Six");
+ else if(digit==7)
+
+console.log("Seven");
+else if(digit==8)
+
+console.log("Eight");
+else if (digit==9)
+
+console.log("Nine");
+
+else 
+
+console.log("Not a digit");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
